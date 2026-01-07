22e78d8bc0d72237d024ae18a60b397b47b08305
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-01-07 10:52:49 +0530

    day 11

---
+++ Delete_node_in_LL_237.java
@@ -0,0 +1,32 @@
+public class Delete_node_in_LL_237{
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+        Delete_node_in_LL_237 solution = new Delete_node_in_LL_237();
+        solution.deleteNode(head.next);
+        for (ListNode curr = head; curr != null; curr = curr.next) {
+            System.out.print(curr.val + " ");
+        }
+
+    }
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+
+    }
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
