ab82094598b587a28c06d5cf6068a67b7eb78a1c
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-01-17 00:37:00 +0530

    day 20

---
+++ LL/Add_2_LL_2.java
@@ -0,0 +1,52 @@
+public class Add_2_LL_2 {
+    static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val=val;
+
+        }
+    }
+    public static void main(String[] args){
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(5);
+        head.next.next.next = new ListNode(9);
+        ListNode head1 = new ListNode(1);
+        head1.next = new ListNode(4);
+        head1.next.next = new ListNode(5);
+        head1.next.next.next = new ListNode(9);
+        Add_2_LL_2 obj = new Add_2_LL_2();
+        head=obj.addTwoNumbers(head, head1);
+        for(ListNode curr=head;curr!=null;curr=curr.next){
+            System.out.print(curr.val+" ");
+        }
+
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans=new ListNode(-1);
+        ListNode head=ans;
+        int carry=0;
+        while(l1!=null || l2!=null){
+            int sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            sum+=carry;
+            carry = sum/10;
+            ListNode temp=new ListNode(sum%10);
+            ans.next=temp;
+            ans=ans.next;
+        }
+        if(carry==1){
+            ans.next=new ListNode(carry);
+            ans=ans.next;
+        }
+        return head.next;
+    }
+}
