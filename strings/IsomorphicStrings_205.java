73adfb394b3587e40f58ad12ffb25b44a5f21b80
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-01-28 10:17:17 +0530

    day 22

--- strings/IsomorphicStrings_205.java
+++ strings/IsomorphicStrings_205.java
@@ -18,4 +18,17 @@ public class IsomorphicStrings_205 {
         }
         return true;
     }
+    public boolean isIsomorphic1(String s, String t) {        
+        if(s.length()!=t.length()) return false;
+        int[] mpps=new int[256];
+        int[] mppt=new int[256];
+        for(int i=0;i<s.length();i++){
+            char cs=s.charAt(i);
+            char ct=t.charAt(i);
+            if(mpps[cs]!=mppt[ct]) return false;
+            mpps[cs]=i+1;
+            mppt[ct]=i+1;           
+        }
+        return true;
+    }
 }
