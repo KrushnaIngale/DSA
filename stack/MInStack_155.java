58b3f2cedac626d73d0b7f044750a6f1d74d738a
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-02-01 10:42:34 +0530

    day 26

--- stack/MInStack_155.java
+++ stack/MInStack_155.java
@@ -41,3 +41,86 @@ public class MInStack_155 {
         System.out.println(st.top());
     }
 }
+// --------------------------------------------------------------------------------
+// class Pair{
+//         int x,y;
+//         Pair(int x,int y){
+//             this.x=x;
+//             this.y=y;
+//         }
+//     }
+// class MinStack {
+//     Stack<Pair> st;
+    
+//     public MinStack() {
+//         st=new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         int min;
+//         if(st.isEmpty()){
+//             min=val;
+//         }else{
+//             min=Math.min(st.peek().y,val);
+//         }
+//         st.push(new Pair(val,min));
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//     }
+    
+//     public int top() {
+//         return st.peek().x;
+//     }
+    
+//     public int getMin() {
+//         return st.peek().y;
+//     }
+// }
+
+// --------------------------------------------------------------------------------------------------------
+// class MinStack {
+//     Stack<Long> st;
+//     Long min;
+//     public MinStack() {
+//         st=new Stack<>();
+//         min=Long.MAX_VALUE;
+//     }
+    
+//     public void push(int value) {
+//         Long val=Long.valueOf(value);
+//         if(st.isEmpty()){
+//             st.push(val);
+//             min=val;
+//         }else{
+//             if(val<min){
+//                 st.push(2*val-min);
+//                 min=val;
+//             }else{
+//                 st.push(val);
+//             }
+//         }
+//     }
+    
+//     public void pop() {
+//         Long val=st.pop();
+//         if(val<min){
+//             min=2*min-val;
+//         }
+//     }
+    
+//     public int top() {
+//         Long val=st.peek();
+//         if(val<min){
+//             return min.intValue();
+//         }else{
+//             return val.intValue();
+//         }
+//     }
+    
+//     public int getMin() {
+//         return min.intValue();
+//     }
+// }
+
