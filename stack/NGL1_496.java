698ce743c3d1073424f6d25f9af7cbc990dcfb55
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-02-02 10:14:30 +0530

    day 27

--- stack/NGL1_496.java
+++ stack/NGL1_496.java
@@ -32,4 +32,29 @@ public class NGL1_496 {
             System.out.print(ans[i]+" ");
         }
     }
+    public int[] nextGreaterElement1(int[] nums1, int[] nums2) {
+        Stack<Integer>st=new Stack<>();
+        int[] ans1=new int[nums1.length];
+        int[] ans2=new int[nums2.length];
+        for(int i=nums2.length-1;i>-1;i--){
+            while(!st.isEmpty() && st.peek()<=nums2[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans2[i]=-1;
+            }else{
+                ans2[i]=st.peek();
+            }
+            st.push(nums2[i]);
+        }
+
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    ans1[i]=ans2[j];
+                }
+            }
+        }
+        return ans1;
+    }
 }
