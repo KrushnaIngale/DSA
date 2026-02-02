9b23de8ca91099561816ca0903eca24cde441b99
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-02-02 10:13:54 +0530

    day 27

---
+++ stack/NGL1_496.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class NGL1_496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer>st=new Stack<>();
+        int[] ans1=new int[nums1.length];
+        HashMap<Integer,Integer>mpp=new HashMap<>();
+        for(int i=nums2.length-1;i>-1;i--){
+            while(!st.isEmpty() && st.peek()<=nums2[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                mpp.put(nums2[i],-1);
+            }else{
+                mpp.put(nums2[i],st.peek());
+            }
+            st.push(nums2[i]);
+        }
+
+        for(int i=0;i<nums1.length;i++){
+            ans1[i]=mpp.get(nums1[i]);
+        }
+        return ans1;
+    }
+    public static void main(String[] args) {
+        int[] nums1={4,1,2};
+        int[] nums2={1,3,4,2};
+        NGL1_496 n=new NGL1_496();
+        int[] ans=n.nextGreaterElement(nums1,nums2);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
