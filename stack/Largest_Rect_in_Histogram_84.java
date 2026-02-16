64fbb2f36c31203143630cdd7432a8958ccc16e7
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-02-16 08:43:59 +0530

    day 32

---
+++ stack/Largest_Rect_in_Histogram_84.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class Largest_Rect_in_Histogram_84 {
+    public static void main(String[] args) {
+        int[] heights={2,1,5,6,2,3};
+        Largest_Rect_in_Histogram_84 l=new Largest_Rect_in_Histogram_84();
+        System.out.println(l.largestRectangleArea(heights));
+    }
+    public int largestRectangleArea(int[] heights) {
+        int[] NSR=getNSR(heights);
+        int[] NSL=getNSL(heights);
+        int maxArea=0;
+        for(int i=0;i<heights.length;i++){
+            maxArea=Math.max(maxArea,(NSR[i]-NSL[i]-1)*heights[i]);
+        }
+        return maxArea;
+    }
+    int[] getNSR(int[] nums){
+        Stack<Integer>st=new Stack<>();
+        int[] ans=new int[nums.length];
+        for(int i=nums.length-1;i>-1;i--){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+            ans[i]=st.isEmpty()?nums.length:st.peek();
+            st.push(i);
+        }
+        return ans;
+    }
+    int[] getNSL(int[] nums){
+        Stack<Integer>st=new Stack<>();
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+            ans[i]=st.isEmpty()?-1:st.peek();
+            st.push(i);
+        }
+        return ans;
+    }
+}
