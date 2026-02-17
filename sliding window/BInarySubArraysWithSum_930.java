06ea190650dbd03a0d940770fb5461372415d5a4
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-02-17 17:04:34 +0530

    day 33

--- sliding window/BInarySubArraysWithSum_930.java
+++ sliding window/BInarySubArraysWithSum_930.java
@@ -6,6 +6,28 @@ public class BInarySubArraysWithSum_930 {
         System.out.println(b.numSubarraysWithSum(nums,goal));
     }
     public int numSubarraysWithSum(int[] nums, int goal) {
+        return lessThanSum(nums,goal)-lessThanSum(nums,goal-1);
+    }
+    int lessThanSum(int[] nums,int goal){
+        if(goal<0) return 0;
+
+        int n=nums.length;
+        int l=0;
+        int r=0;
+        int sum=0;
+        int count=0;
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
+            }
+            count+=(r-l+1);
+            r++;
+        }
+        return count;
+    }
+    public int numSubarraysWithSum1(int[] nums, int goal) {
         int count=0;
         for(int i=0;i<nums.length;i++){
             int sum=0;
