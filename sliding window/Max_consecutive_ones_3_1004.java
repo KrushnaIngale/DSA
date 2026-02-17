8513e206fb04741a3fa65c368a684da297ce0ad6
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-02-17 09:36:01 +0530

    day 33

--- sliding window/Max_consecutive_ones_3_1004.java
+++ sliding window/Max_consecutive_ones_3_1004.java
@@ -18,6 +18,27 @@ public class Max_consecutive_ones_3_1004 {
             if(nums[r]==0){
                 zeros++;
             }
+            if(zeros>k){
+                if(nums[l]==0){
+                    zeros--;
+                }
+                l++;
+            }
+            max=Math.max(max,r-l+1);
+            r++;
+        }
+        return max;
+    }
+    public int longestOnes2(int[] nums, int k) {
+        int max=0;
+        int n=nums.length;
+        int l=0;
+        int r=0;
+        int zeros=0;
+        while(r<n){
+            if(nums[r]==0){
+                zeros++;
+            }
             while(zeros>k){
                 if(nums[l]==0){
                     zeros--;
