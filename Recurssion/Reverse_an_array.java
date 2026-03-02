1c9c3032253b6ad3ea316d16e168f1255a53c80b
KrushnaIngale <krushna.g.i.37@gmail.com>
2026-03-02 12:47:00 +0530

    day 45

--- Recurssion/Reverse_an_array.java
+++ Recurssion/Reverse_an_array.java
@@ -3,6 +3,12 @@ public class Reverse_an_array{
         int[] arr={1,2,3,4,2};
 
         reverse(arr,arr.length-1);
+        System.out.println();
+        
+        reverseInplace(0,arr.length-1,arr);
+        for(int i:arr){
+            System.out.print(i);
+        }
     }
     static void reverse(int[] arr,int i){
         if(i<0){
@@ -10,5 +16,15 @@ public class Reverse_an_array{
         }
         System.out.print(arr[i]);
         reverse(arr,i-1);
+    }
+
+    static void reverseInplace(int i,int j,int[] arr){
+            if(i>=j)    return;
+
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            
+            reverseInplace(i+1,j-1,arr);
     }
 }
